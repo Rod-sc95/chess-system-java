a07bc9e87a7e37bc472daf91e789d7b49a8b9663
Rod-sc95 <rodrigo_sc95@hotmail.com>
2021-04-06 08:03:45 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Board.java
@@ -0,0 +1,16 @@
+package boardgame;
+
+public class Board {
+
+	private int row; // linhas do tabuleiro
+	private int columns; // Colunas do tabuleira
+	private Piece [][] pieces; // Matriz de peças
+	
+	public Board(int row, int columns) {
+		super();
+		this.row = row;
+		this.columns = columns;
+	}
+	
+	
+}
