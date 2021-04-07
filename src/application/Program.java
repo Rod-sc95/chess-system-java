81b5ac43ed311635b7080edf5d7f0050530cfcea
Rod-sc95 <rodrigo_sc95@hotmail.com>
2021-04-07 08:46:07 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,33 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		// Instanciação da partida
 		ChessMatch chessMatch = new ChessMatch();
-		// Função que imprimi as peças da partida atraves da class UI pelo metodo printBoard dentro da class
-		UI.printBoard(chessMatch.getPieces());
-
+		
+		while(true) {
+			// Função que imprimi as peças da partida atraves da class UI pelo metodo printBoard dentro da class
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
+		
 	}
 
 }
